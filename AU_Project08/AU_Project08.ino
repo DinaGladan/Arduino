9b204ee3ff08ffea59ff3c9011a1375f18644eff
DinaGladan <147320265+DinaGladan@users.noreply.github.com>
2025-08-19 12:37:25 +0200

    Paljenje i gasenje svjetala pomocu tilt switcha

---
+++ AU_Project08/AU_Project08.ino
@@ -0,0 +1,36 @@
+int switchPin = 8;
+unsigned long previousTime =0;
+int switchState = 0;
+int prevSwitchState= 0; 
+int led =  2; //koja ledica je iduca da se upali
+long interval = 2000; //koliko dugo da se iduca ledica upali
+void setup() {
+  // put your setup code here, to run once:
+  for(int x = 2; x<8; x++){
+    pinMode(x, OUTPUT);
+  }
+  pinMode(switchPin, INPUT);
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  unsigned long currentTime = millis();
+  if (currentTime - previousTime > interval){
+    previousTime = currentTime;
+    digitalWrite(led, HIGH);
+    led++;
+  }
+  if(led == 9){
+    prevSwitchState =  0;
+  }
+  switchState = digitalRead(switchPin);
+  if(switchState != prevSwitchState){
+    for(int x = 2; x<8; x++){
+    digitalWrite(x, LOW);
+    }
+    led = 2;
+    previousTime = currentTime;
+  }
+  prevSwitchState = switchState;
+}
